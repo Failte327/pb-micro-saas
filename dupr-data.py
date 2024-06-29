ef1ba3f52e13c5ab73f7f32df06a1e5c99125294
Timothy Twelker <timothy.twelker@gmail.com>
2024-06-29 10:56:39 -0600

    Expanded request_handler to include both posts and gets in separate endpoints, worked on dupr-data.py, got search function > pulling ratings working

--- dupr-data.py
+++ dupr-data.py
@@ -1,13 +1,39 @@
 import requests
 
-player_id = "16OGL0"
+player_id = "8105602894"
 
-search_url = f"player/v1.0/{player_id}"
+search_url = f"player/v1.0/search"
 
-data = {
-    "endpoint": search_url
+# Change query to whatever the player's name is
+query_data = {
+    "limit":10,
+    "offset":0,
+    "query":"*",
+    "exclude":[],
+    "includeUnclaimedPlayers":True,
+    "filter":{
+        "lat":40.114955,
+        "lng":-111.654923,
+        "rating":{
+            "maxRating":None,
+            "minRating":None
+            },
+        "locationText":""
+        }
 }
 
-response = requests.post("http://127.0.0.1:10000/make_request", json=data)
+data = {
+    "endpoint": search_url,
+    "data": query_data
+}
 
-print(response)
+response = requests.post("http://127.0.0.1:5000/make_post_request", json=data)
+
+search_result = response.json()
+
+# Goes through the matching search results, picks the first one, grabs their doubles rating
+doubles_rating = search_result["result"]["hits"][0]["ratings"]["doubles"]
+player_name = search_result["result"]["hits"][0]["fullName"]
+
+print(player_name)
+print(doubles_rating)
--- request_handler.py
+++ request_handler.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 auth_token = ""
 
-@app.route('/make_request', methods=['POST'])
-def make_request():
+@app.route('/make_get_request', methods=['POST'])
+def get():
     global auth_token
     base_url = "https://api.dupr.gg/"
     auth_endpoint = "auth/v1.0/login"
@@ -16,13 +16,36 @@ def make_request():
     if auth_token == "":
         auth_url = f"{base_url}{auth_endpoint}"
         # credentials go here
-        data = {}
+        data = {
+        }
         response = requests.post(auth_url, json=data)
         json = response.json()
         auth_token = json["result"]["accessToken"]
         print(json)
         print(auth_token)
-    # requested = requests.get(f"{base_url}{provided_endpoint}", headers={"Authorization": f"Bearer {auth_token}"})
-    # print(requested.json())
-    return auth_token
+    requested = requests.get(f"{base_url}{provided_endpoint}", headers={"Authorization": f"Bearer {auth_token}"})
+    print(requested.json())
+    return requested.json()
+
+@app.route('/make_post_request', methods=['POST'])
+def post():
+    global auth_token
+    base_url = "https://api.dupr.gg/"
+    auth_endpoint = "auth/v1.0/login"
+    provided_endpoint = request.json.get("endpoint")
+    provided_data = request.json.get("data")
+    print(provided_endpoint)
+    if auth_token == "":
+        auth_url = f"{base_url}{auth_endpoint}"
+        # credentials go here
+        data = {
+        }
+        response = requests.post(auth_url, json=data)
+        json = response.json()
+        auth_token = json["result"]["accessToken"]
+        print(json)
+        print(auth_token)
+    requested = requests.post(f"{base_url}{provided_endpoint}", headers={"Authorization": f"Bearer {auth_token}"}, json=provided_data)
+    print(requested.json())
+    return requested.json()
     
