1b624a88fc64e09985e17ad0063c7ed74ded5676
TC Twelker <timothy.twelker@securitymetrics.com>
2024-07-31 14:58:47 -0600

    Removed angular, going to rehaul in bootstrap

--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,15 @@
-from flask import Flask, request
+from bracketool.single_elimination import SingleEliminationGen
+from bracketool.domain import *
+from flask import Flask, request, render_template
 import requests
 
 app = Flask(__name__)
 
 auth_token = ""
+
+@app.route('/')
+def home():
+    return render_template("home.html")
 
 @app.route('/make_get_request', methods=['POST'])
 def get():
@@ -46,4 +52,65 @@ def post():
     print(requested.json())
     return requested.json()
 
-    
+@app.route('/generate_bracket')
+def generate_bracket():
+    print("Fetching player ratings...")
+    search_url = f"player/v1.0/search"
+    competitor_list = []
+    default_rating = float("3.5")
+
+    # Sample list from a recent open play event
+    player_list = ['Timothy Twelker', 'Sean Halbleib', 'David Andersen', 'Garry Oborn']
+
+    for i in player_list:
+        query_data = {
+            "limit":10,
+            "offset":0,
+            "query":f"{i}",
+            "exclude":[],
+            "includeUnclaimedPlayers":True,
+            "filter":{
+                "lat":40.114955,
+                "lng":-111.654923,
+                "rating":{
+                    "maxRating":None,
+                    "minRating":None
+                    },
+                "locationText":""
+                }
+        }
+
+        data = {
+            "endpoint": search_url,
+            "data": query_data
+        }
+
+        response = requests.post("http://127.0.0.1:5000/make_post_request", json=data)
+
+        search_result = response.json()
+
+        # Goes through the matching search results, picks the first one, grabs their doubles rating
+        if search_result["result"]["hits"] != []:
+            doubles_rating = search_result["result"]["hits"][0]["ratings"]["doubles"]
+            if doubles_rating == "NR":
+                doubles_rating = default_rating
+            else:
+                doubles_rating = float(search_result["result"]["hits"][0]["ratings"]["doubles"])
+            player_name = search_result["result"]["hits"][0]["fullName"]
+            new_comp = Competitor(name=player_name, team=player_name, rating=doubles_rating)
+            competitor_list.append(new_comp)
+        else:
+            print(f"No entry found for {i}")
+
+    gen = SingleEliminationGen(
+        use_three_way_final=False,
+        third_place_clash=True,
+        use_rating=True,
+        use_teams=True,
+        random_seed=42
+    )
+
+    output = gen.generate(competitor_list)
+
+    print(output.rounds[0])
+    return output.rounds[0]
