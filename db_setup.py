c100a67f0eb9a1905428525a3e4bb597150ef2d4
Timothy Twelker <timothy.twelker@gmail.com>
2024-08-09 00:01:31 -0600

    Added db for court pooling

---
+++ db_setup.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine, Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import database_exists, create_database
+
+engine = create_engine("sqlite:///court_pool.db")
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+print(database_exists(engine.url))
+
+class AvailableCourts():
+    __tablename__ = "available_courts"
+
+    id = Column(Integer, primary_key=True)
+    available_courts = Column(Integer)
+
