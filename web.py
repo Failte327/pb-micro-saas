aebf857c1baf1354d48b56013a11ca88403060d8
TC Twelker <timothy.twelker@securitymetrics.com>
2024-08-28 16:33:48 -0600

    Created interface that will parse a csv and spit out dupr data for the players in the csv, need to try it on sample data like from one of the tournaments I've played in to see what problems come up

--- web.py
+++ web.py
@@ -11,6 +11,7 @@ auth_token = ""
 def home():
     return render_template("home.html")
 
+# dupr auth
 @app.route('/make_get_request', methods=['POST'])
 def get():
     global auth_token
@@ -25,8 +26,8 @@ def get():
         response = requests.post(auth_url, json=data)
         json = response.json()
         auth_token = json["result"]["accessToken"]
-        print(json)
-        print(auth_token)
+        if auth_token != "":
+            print(f"auth token: {auth_token}")
     requested = requests.get(f"{base_url}{provided_endpoint}", headers={"Authorization": f"Bearer {auth_token}"})
     print(requested.json())
     return requested.json()
@@ -46,11 +47,70 @@ def post():
         response = requests.post(auth_url, json=data)
         json = response.json()
         auth_token = json["result"]["accessToken"]
-        print(json)
-        print(auth_token)
+        if auth_token != "":
+            print(f"auth token: {auth_token}")
     requested = requests.post(f"{base_url}{provided_endpoint}", headers={"Authorization": f"Bearer {auth_token}"}, json=provided_data)
-    print(requested.json())
     return requested.json()
+
+@app.route('/parse_csv', methods=["POST"])
+def parse_csv():
+    print("Fetching player ratings...")
+    search_url = f"player/v1.0/search"
+    competitor_list = []
+    # player_list = request.form.get("playername").split(",")
+    data = request.files.get("csv_upload").read().decode("utf-8")
+    player_list = data.split(",")
+    counter = 0
+    for i in player_list:
+        new = i.strip().lower()
+        player_list[counter] = new
+        counter = counter + 1
+
+    print(player_list)
+
+    for i in player_list:
+        query_data = {
+            "limit":10,
+            "offset":0,
+            "query":f"{i}",
+            "exclude":[],
+            "includeUnclaimedPlayers":True,
+            "filter":{
+                "lat":40.114955,
+                "lng":-111.654923,
+                "rating":{
+                    "maxRating":None,
+                    "minRating":None
+                    },
+                "locationText":""
+                }
+        }
+
+        data = {
+            "endpoint": search_url,
+            "data": query_data
+        }
+
+        response = requests.post("http://127.0.0.1:10000/make_post_request", json=data)
+
+        print(response)
+        search_result = response.json()
+
+        # Goes through the matching search results, picks the first one, grabs their doubles rating
+        if search_result["result"]["hits"] != []:
+            doubles_rating = search_result["result"]["hits"][0]["ratings"]["doubles"]
+            if doubles_rating == "NR":
+                doubles_rating = "Unknown"
+            else:
+                doubles_rating = float(search_result["result"]["hits"][0]["ratings"]["doubles"])
+            player_name = search_result["result"]["hits"][0]["fullName"]
+            competitor_list.append({player_name: doubles_rating})
+        else:
+            print(f"No entry found for {i}")
+        
+        print(competitor_list)
+    
+    return competitor_list
 
 # @app.route('/generate_bracket', methods=["POST"])
 # def generate_bracket():
