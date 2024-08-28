54b60befc33dbaa1c2e5d57fda5b3755030909fb
TC Twelker <timothy.twelker@securitymetrics.com>
2024-08-28 15:31:32 -0600

    Restructured project for a new direction, removed creds from some files

--- web.py
+++ web.py
@@ -21,10 +21,7 @@ def get():
     if auth_token == "":
         auth_url = f"{base_url}{auth_endpoint}"
         # credentials go here
-        data = {
-            "email": "timothy.twelker@gmail.com",
-            "password": "Shealyn15!"
-        }
+        data = {}
         response = requests.post(auth_url, json=data)
         json = response.json()
         auth_token = json["result"]["accessToken"]
@@ -45,10 +42,7 @@ def post():
     if auth_token == "":
         auth_url = f"{base_url}{auth_endpoint}"
         # credentials go here
-        data = {
-            "email": "timothy.twelker@gmail.com",
-            "password": "Shealyn15!"
-        }
+        data = {}
         response = requests.post(auth_url, json=data)
         json = response.json()
         auth_token = json["result"]["accessToken"]
@@ -58,82 +52,82 @@ def post():
     print(requested.json())
     return requested.json()
 
-@app.route('/generate_bracket', methods=["POST"])
-def generate_bracket():
-    print("Fetching player ratings...")
-    search_url = f"player/v1.0/search"
-    competitor_list = []
-    default_rating = float("3.5")
-    player_list = request.form.get("playername").split(",")
-    counter = 0
-    for i in player_list:
-        new = i.strip().lower()
-        player_list[counter] = new
-        counter = counter + 1
+# @app.route('/generate_bracket', methods=["POST"])
+# def generate_bracket():
+#     print("Fetching player ratings...")
+#     search_url = f"player/v1.0/search"
+#     competitor_list = []
+#     default_rating = float("3.5")
+#     player_list = request.form.get("playername").split(",")
+#     counter = 0
+#     for i in player_list:
+#         new = i.strip().lower()
+#         player_list[counter] = new
+#         counter = counter + 1
 
-    print(player_list)
+#     print(player_list)
 
-    for i in player_list:
-        query_data = {
-            "limit":10,
-            "offset":0,
-            "query":f"{i}",
-            "exclude":[],
-            "includeUnclaimedPlayers":True,
-            "filter":{
-                "lat":40.114955,
-                "lng":-111.654923,
-                "rating":{
-                    "maxRating":None,
-                    "minRating":None
-                    },
-                "locationText":""
-                }
-        }
+#     for i in player_list:
+#         query_data = {
+#             "limit":10,
+#             "offset":0,
+#             "query":f"{i}",
+#             "exclude":[],
+#             "includeUnclaimedPlayers":True,
+#             "filter":{
+#                 "lat":40.114955,
+#                 "lng":-111.654923,
+#                 "rating":{
+#                     "maxRating":None,
+#                     "minRating":None
+#                     },
+#                 "locationText":""
+#                 }
+#         }
 
-        data = {
-            "endpoint": search_url,
-            "data": query_data
-        }
+#         data = {
+#             "endpoint": search_url,
+#             "data": query_data
+#         }
 
-        response = requests.post("http://127.0.0.1:10000/make_post_request", json=data)
+#         response = requests.post("http://127.0.0.1:10000/make_post_request", json=data)
 
-        print(response)
-        search_result = response.json()
+#         print(response)
+#         search_result = response.json()
 
-        # Goes through the matching search results, picks the first one, grabs their doubles rating
-        if search_result["result"]["hits"] != []:
-            doubles_rating = search_result["result"]["hits"][0]["ratings"]["doubles"]
-            if doubles_rating == "NR":
-                doubles_rating = default_rating
-            else:
-                doubles_rating = float(search_result["result"]["hits"][0]["ratings"]["doubles"])
-            player_name = search_result["result"]["hits"][0]["fullName"]
-            new_comp = Competitor(name=player_name, team=player_name, rating=doubles_rating)
-            competitor_list.append(new_comp)
-        else:
-            print(f"No entry found for {i}")
+#         # Goes through the matching search results, picks the first one, grabs their doubles rating
+#         if search_result["result"]["hits"] != []:
+#             doubles_rating = search_result["result"]["hits"][0]["ratings"]["doubles"]
+#             if doubles_rating == "NR":
+#                 doubles_rating = default_rating
+#             else:
+#                 doubles_rating = float(search_result["result"]["hits"][0]["ratings"]["doubles"])
+#             player_name = search_result["result"]["hits"][0]["fullName"]
+#             new_comp = Competitor(name=player_name, team=player_name, rating=doubles_rating)
+#             competitor_list.append(new_comp)
+#         else:
+#             print(f"No entry found for {i}")
 
-    gen = SingleEliminationGen(
-        use_three_way_final=False,
-        third_place_clash=True,
-        use_rating=True,
-        use_teams=True,
-        random_seed=42
-    )
+#     gen = SingleEliminationGen(
+#         use_three_way_final=False,
+#         third_place_clash=True,
+#         use_rating=True,
+#         use_teams=True,
+#         random_seed=42
+#     )
 
-    output = gen.generate(competitor_list)
+#     output = gen.generate(competitor_list)
 
-    print(output.rounds[0])
+#     print(output.rounds[0])
     
-    clashes = {}
-    clash_number = 0
-    for n in output.rounds[0]:
-        competitor_a = n.competitor_a.name
-        if n.competitor_b is not None:
-            competitor_b = n.competitor_b.name
-        else:
-            competitor_b = "BYE"
-        clashes[clash_number] = f"{competitor_a},{competitor_b}"
-        clash_number = clash_number + 1
-    return clashes
+#     clashes = {}
+#     clash_number = 0
+#     for n in output.rounds[0]:
+#         competitor_a = n.competitor_a.name
+#         if n.competitor_b is not None:
+#             competitor_b = n.competitor_b.name
+#         else:
+#             competitor_b = "BYE"
+#         clashes[clash_number] = f"{competitor_a},{competitor_b}"
+#         clash_number = clash_number + 1
+#     return clashes
